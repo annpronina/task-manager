e1c404a5a203e0625b0962c1713253bfa264f05e
unknown <your@email.example>
2025-04-08 10:33:33 +0300

    authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,18 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
-        return 'register';
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|max:225',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+       $user = User::create($fields);
+
+       $token = $user->createToken($request->name);
+
+       return [
+        'user' => $user,
+        'token' => $token
+       ];
     }
 
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         return 'login';
     }
 
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
         return 'logout';
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -9,4 +10,4 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/login', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
